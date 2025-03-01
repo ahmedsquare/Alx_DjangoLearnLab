2b0fbe2ec0f0b23518b7425d6ca99deecb01152a
Ahmed <ahmedsquare@aucegypt.edu>
2025-03-01 17:37:58 +0200

    Final 3. Implement Role-Based Access Control in Django

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import list_books, LibraryDetailView
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views import admin_view
+from .admin_view import admin_view  # Import from the correct file
 from .views import librarian_view
 from .views import member_view 
 
