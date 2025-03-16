1788a66fdd9a5b2649f980294a5b12b444c62d5d
Ahmed <ahmedsquare@aucegypt.edu>
2025-03-16 21:46:22 +0200

    1. Building Custom Views and Generic Views in Django REST Framework

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book #replace with your working model
+from .serializers import BookSerializer # replace with your project's serializer
 
-# Create your views here.
+class CustomBookCreateView(generics.CreateAPIView):
+# can be any name, ensure to align with your project as this is sample exampls 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class CustomBookListView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
