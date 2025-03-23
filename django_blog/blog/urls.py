1ece5bbad93401fe207de2e25b0c8a8ee66a1416
Ahmed <ahmedsquare@aucegypt.edu>
2025-03-23 21:03:37 +0200

    1. Implementing the Blogs User Authentication System

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
 from . import views
-from .views import login_view
-from .views import register_view  # Ensure you import your view
 
 urlpatterns = [
-    path('', views.home, name='home'),  # Define a default route for the blog
-    path('posts/', views.posts, name='posts'),  # Add this line
-    path('login/', login_view, name='login'),
-    path('register/', register_view, name='register'),
-
+    path('', views.home, name='home'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('posts/', views.posts, name='posts'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib import messages
+from .forms import UserRegisterForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     return render(request, 'blog/base.html')  # Ensure 'blog/home.html' exists
@@ -11,3 +15,24 @@ def posts(request):
 
 def register_view(request):
     return render(request, 'blog/register.html')  # Ensure this template exists
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, 'Account created successfully!')
+            return redirect('profile')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+def user_logout(request):
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return redirect('login')
+
+@login_required
+def profile(request):
+    return render(request, 'blog/profile.html')
