c35e8211bc812453d9a605639b8dbe9f165a0937
Ahmed <ahmedsquare@aucegypt.edu>
2025-03-23 21:27:28 +0200

    2. Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,15 +9,17 @@ from .views import (
 )
 
 urlpatterns = [
+    # Authentication
     path('', views.home, name='home'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.user_logout, name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
-    path('posts/', views.posts, name='posts'),
-    path('postslist/', PostListView.as_view(), name='post-list'),  # List all posts
-    path('postslist/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
-    path('postslist/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a post
-    path('postslist/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit a post
-    path('postslist/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
+
+    # Blog Post CRUD Routes
+    path('post/', PostListView.as_view(), name='post'),  # List all posts
+    path('post/new/', PostCreateView.as_view(), name='post-create'),  # Create a new post
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),  # View a post
+    path('post/<int:pk>/edit/', PostUpdateView.as_view(), name='post-update'),  # Edit a post
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # Delete a post
 ]
